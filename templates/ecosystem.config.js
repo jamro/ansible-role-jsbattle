0419494a986c583d572fe19a49cad166cdcef845
Krzysztof Jamroz <jamro@jamro.net>
2019-03-10 19:20:23 +0100

    use PM2 externally to support jsbattle 2.2.0

--- templates/ecosystem.config.js
+++ templates/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'jsbattle',
-    script: "jsbattle.js",
+    script: "{{ jsbattle_home }}/jsbattle/node_modules/jsbattle/dist/jsbattle.js",
     cwd: __dirname,
     args: "start -h {{ jsbattle_address }} -p {{ jsbattle_port }} -l info -d {{ jsbattle_home }}/jsbattle-data",
     instances: 1,
