567d68dc01c6192cff24dcce2a12258db0aeaecc
Krzysztof Jamroz <jamro@jamro.net>
2020-01-21 14:57:39 +0100

    keep data in separate location

--- templates/jsbattle.sh
+++ templates/jsbattle.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-{{ jsbattle_home }}/jsbattle/node_modules/jsbattle/dist/jsbattle.js start -h {{ jsbattle_address }} -p {{ jsbattle_port }} -l info -d {{ jsbattle_home }}/jsbattle-data --gaCode {{ jsbattle_ga_code }}
+{{ jsbattle_home }}/jsbattle/node_modules/jsbattle/dist/jsbattle.js start -h {{ jsbattle_address }} -p {{ jsbattle_port }} -l info -d {{ jsbattle_data }}/jsbattle-data --gaCode {{ jsbattle_ga_code }}
